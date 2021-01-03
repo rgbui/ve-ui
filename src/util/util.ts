353197660c8a7650574a4dea636490ed10b04f3d
kanhai <760489033@qq.com>
2021-01-03 23:08:03 +0800

    util

---
+++ src/util/util.ts
@@ -0,0 +1,115 @@
+import * as short from 'short-uuid';
+import moment from "moment";
+export var util = {
+    inherit(Mix, ...mixins) {
+        function copyProperties(target, source) {
+            for (let key of Reflect.ownKeys(source)) {
+                if (key !== "constructor"
+                    && key !== "prototype"
+                    && key !== "name"
+                ) {
+                    let desc = Object.getOwnPropertyDescriptor(source, key);
+                    Object.defineProperty(target, key, desc);
+                }
+            }
+        }
+        for (let mixin of mixins) {
+            copyProperties(Mix, mixin);
+            copyProperties(Mix.prototype, mixin.prototype);
+        }
+        return Mix;
+    },
+    guid() {
+        return short.generate();
+    },
+    clone<T>(json: T): T {
+        try {
+            let copy = (o) => {
+                if (Array.isArray(o)) {
+                    return o.map(x => copy(x));
+                }
+                else if (typeof o == 'undefined') {
+                    return o;
+                }
+                else if (o === null) {
+                    return 0;
+                }
+                else if (typeof o == 'object') {
+                    var json = {};
+                    for (let n in o) {
+                        json[n] = copy(o[n])
+                    }
+                    return json;
+                }
+                else return o;
+            }
+            return copy(json);
+        }
+        catch (e) {
+            throw e;
+        }
+    },
+    valueIsEqual(a, b) {
+        let equal = (o, n) => {
+            if (n == null) {
+                if (o != null) return false;
+            }
+            else if (typeof n == 'undefined') {
+                if (o !== undefined) return false;
+            }
+            else if (Array.isArray(n)) {
+                if (!Array.isArray(o)) return false;
+                if (o.length != n.length) return false;
+                for (let i = 0; i < n.length; i++) {
+                    if (!equal(o[i], n[i])) return false;
+                }
+            }
+            else if (n instanceof Date) {
+                if (!(o instanceof Date)) return false;
+                if (n.getTime() != o.getTime()) return false;
+            }
+            else if (typeof n == 'object') {
+                if (typeof o != 'object') return false;
+                if (Object.keys(n).length != Object.keys(o).length) return false;
+                for (var x in n) {
+                    if (!equal(o[x], n[x])) {
+                        return false;
+                    }
+                }
+            }
+            else if (o !== n) {
+                return false;
+            }
+            return true;
+        }
+        return equal(a, b);
+    },
+    listToTree(list: any[], id: string, parentName: string, childName: string) {
+        var array: any[] = [];
+        for (let i = 0; i < list.length; i++) {
+            var item = list[i];
+            if (item[parentName]) {
+                var parentItem = list.find(x => x[id] == item[parentName]);
+                if (parentItem) {
+                    if (!Array.isArray(parentItem[childName])) parentItem[childName] = [];
+                    parentItem[childName].push(item);
+                }
+                else {
+                    console.log('not found parent id', item, item[parentName]);
+                    array.push(item);
+                }
+            }
+            else array.push(item)
+        }
+        return array;
+    },
+    toFormateDate(date: string | Date) {
+        if (date instanceof Date) {
+            return moment(date).format("YYYY-MM-DD hh:mm:ss");
+        }
+        else if (typeof date == 'string' && date.indexOf('T') > -1) {
+            return moment(date).format("YYYY-MM-DD hh:mm:ss");
+        }
+        else return date;
+    }
+}
